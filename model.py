3e8192abb158c33323399c1a1d85a0be6ef63c18
HongdaChen <89034523@qq.com>
2020-08-20 15:35:02 +0800

    first commit

---
+++ model.py
@@ -0,0 +1,121 @@
+import time
+import pickle
+import argparse
+import numpy as np
+from preprocess_data import Process  # 自定义的，在同级目录
+import matplotlib.pyplot as plt
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.linear_model import LogisticRegression
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.neural_network import MLPClassifier
+
+from sklearn.model_selection import train_test_split, cross_val_score
+
+base_train_sum = r'./data/base_train_sum.csv'
+base_verify1 = r'./data/base_verify1.csv'
+knowledge_train = r'./data/knowledge_train_sum.csv'
+paient_information_verify1 = r'./data/paient_information_verify1.csv'
+money_report_train_sum = r'./data/money_report_train_sum.csv'
+money_information_verify1 = r'./data/money_information_verify1.csv'
+year_report_train_sum = r'./data/year_report_train_sum.csv'
+year_report_verify1 = r'./data/year_report_verify1.csv'
+
+parser = argparse.ArgumentParser(description="choose mode")
+parser.add_argument('--model', type=str, default='knn', help='choose which model to do')
+parser.add_argument('--mode', type=str, default='train',
+                    help='choose train mode or parameters(only knn and random_forest) mode')
+parser.add_argument('--feature', type=int, default=2, help='choose the number of feature you would like to use')
+args = parser.parse_args()
+
+n = args.model
+momo = args.mode
+fe = args.feature
+# ########################################## KNN ###################################################
+if n == 'knn':
+    p = Process(base_train_sum, knowledge_train, money_report_train_sum, year_report_train_sum, features=fe)
+    res = p.alpha_process_csv(base_verify1, paient_information_verify1, money_information_verify1, year_report_verify1)
+    y = res['flag'].values
+    X = res.drop('flag', axis=1)
+    train_X, val_X, train_y, val_y = train_test_split(X, y, test_size=0.3, random_state=0)
+
+    if momo == 'train':
+        knn = KNeighborsClassifier(n_neighbors=4, weights='distance')
+        knn.fit(X, y)
+        acc1 = knn.score(val_X, val_y)
+        pickle.dump(knn, open("model_knn.pkl", "wb"))
+        model =  pickle.load(open("model_knn.pkl", "rb"))
+        print(acc1)
+        
+    elif momo == 'parameter':
+        error = []
+        for k in range(1, 14):
+            knn = KNeighborsClassifier(n_neighbors=k, weights='distance')
+            score = cross_val_score(knn, X, y, cv=6, scoring="accuracy").mean()
+            error.append(1-score)
+
+        plt.plot(np.arange(1, 14), error)
+        plt.xlabel("k")
+        plt.ylabel("loss")
+        plt.title("KNN")
+        plt.show()
+
+# ############################################ 逻辑回归###############################################
+elif n == 'logistic_regression':
+    p = Process(base_train_sum, knowledge_train, money_report_train_sum, year_report_train_sum, features=fe)
+    res = p.alpha_process_csv(base_verify1, paient_information_verify1, money_information_verify1, year_report_verify1)
+    y = res['flag'].values
+    X = res.drop('flag', axis=1)
+    train_X, val_X, train_y, val_y = train_test_split(X, y, test_size=0.3, random_state=0)
+
+    lr = LogisticRegression()
+    lr.fit(X, y)
+    acc2 = lr.score(val_X, val_y)
+    pickle.dump(lr, open("model_logistic.pkl", "wb"))
+    model =  pickle.load(open("model_logistic.pkl", "rb"))
+    print(acc2)
+
+# ############################################# 神经网络 ###########################################
+elif n == 'neural_network':
+    p = Process(base_train_sum, knowledge_train, money_report_train_sum, year_report_train_sum, features=fe)
+    res = p.alpha_process_csv(base_verify1, paient_information_verify1, money_information_verify1, year_report_verify1)
+    y = res['flag'].values
+    X = res.drop('flag', axis=1)
+    train_X, val_X, train_y, val_y = train_test_split(X, y, test_size=0.3, random_state=0)
+
+    clf = MLPClassifier(solver='adam', alpha=1e-3, hidden_layer_sizes=(20, 10),
+                        random_state=1)
+    clf.fit(X, y)
+    acc3 = clf.score(val_X, val_y)
+    pickle.dump(clf, open("model_neural.pkl", "wb"))
+    model =  pickle.load(open("model_neural.pkl", "rb"))
+    print(acc3)
+
+# ########################################## 随机森林 ##############################################
+elif n == 'random_forest':
+    p = Process(base_train_sum, knowledge_train, money_report_train_sum, year_report_train_sum,
+                standard=True, features=fe)  # 不进行标准化
+    res = p.alpha_process_csv(base_verify1, paient_information_verify1, money_information_verify1, year_report_verify1)
+    y = res['flag'].values
+    X = res.drop('flag', axis=1)
+    train_X, val_X, train_y, val_y = train_test_split(X, y, test_size=0.3, random_state=0)
+
+    if momo == 'train':
+        forest = RandomForestClassifier(n_estimators=22)
+        forest.fit(X, y)
+        acc4 = forest.score(val_X, val_y)
+        pickle.dump(forest, open("model_forest.pkl", "wb"))
+        model =  pickle.load(open("model_forest.pkl", "rb"))
+        print(acc4)
+    elif momo == 'parameter':
+        acc = []
+        for k in range(10, 30):
+            forest = RandomForestClassifier(n_estimators=k)
+            score = cross_val_score(forest, X, y, cv=10, scoring="accuracy").mean()
+            acc.append(score)
+        print('最优score：', max(acc), '最优n_estimator:', ([*range(10, 30)][acc.index(max(acc))]))
+        plt.plot(range(10, 30), acc)
+        plt.xlabel("decision_tree_number")
+        plt.ylabel("accuracy")
+        plt.title("RandomForest")
+        plt.show()
+
---
+++ run.py
@@ -0,0 +1,88 @@
+from flask import Flask, make_response, request, render_template
+from preprocess_data import Process  # 自定义的，在同级目录
+import pickle
+import numpy as np
+import os # 用于在本文件中运行bash命令
+
+app = Flask(__name__)
+temp = []
+ff = ['file1', 'file2', 'file3', 'file4']
+# 执行命令，生成模型
+# os.system('python model.py --model random_forest --mode train --feature 7')
+model = pickle.load(open('model_forest.pkl', 'rb'))
+# os.system('python model.py --model logistic_regression --mode train --feature 2')
+model_logistic = pickle.load(open('model_logistic.pkl', 'rb'))
+
+
+@app.route('/')
+def form():
+    return render_template('mini.html')
+
+
+@app.route('/predict', methods=["POST"])
+def predict():
+    global decision
+    decision = request.form.get('dec')
+    if decision == "输入特征":
+        return render_template('max.html')
+    elif decision == "可视化展":
+        return  render_template('view.html')
+    else:
+        return render_template('index.html')
+
+
+@app.route('/letter', methods=['POST'])
+def letter():
+    for i in range(4):
+        file = request.files[ff[i]]
+        temp.append('./temp_data/' + file.filename)
+        file.save(temp[i])
+    # 这里和生成模型那里的参数要保持一致
+    p = Process(temp[0], temp[1], temp[2], temp[3], features=7)
+    res = p.beta_process_csv()
+    # print(res)
+    train_data = res[['纳税总额', '净利润', '注册资本', '负债总额', '从业人数', '营业总收入', '利润总额']]
+    prediction = model.predict(train_data)
+    res['flag'] = prediction
+    res = res[['flag']]
+    if decision == '批量查询':
+        res.to_csv(r"temp_data/many.csv")
+        with open(r"temp_data/many.csv") as fp:
+            output = fp.read()
+        response = make_response(output)
+        response.headers["Content-Disposition"] = "attachment; filename=many.csv"
+        return response
+    else:
+        # print(res)
+        if res.iloc[0][0] == 1:
+            yn = '是'
+        else:
+            yn = '不是'
+        print(res.index)
+        return render_template('index.html', result='ID:{}'.format(res.index.values),
+                               description=yn, sss='僵尸企业')
+
+
+@app.route('/former', methods=['POST'])
+def former():
+    float_features = [float(x) for x in request.form.values()]
+    final_features = [np.array(float_features)]
+    pprediction = model_logistic.predict(final_features)
+    pro = model_logistic.predict_proba(final_features).T[1][0]
+    prob = 100 * round(pro, 2)
+    ooutput = round(pprediction[0], 2)
+    if ooutput == 0:
+        uou = '非僵尸企业'
+    else:
+        uou = '僵尸企业'
+    return render_template('max.html', prediction_text='模型判断结果为：' + uou, data='纳税总额为{}、净利润为{}的企业'.format(float_features[0],float_features[1]),probability='是僵尸企业的概率：{}%'.format(prob))
+
+
+@app.route('/wowk', methods=['POST', 'GET'])
+def wowk():
+    return render_template('mini.html')
+
+
+if __name__ == "__main__":
+    app.run(host="172.17.0.3",port='80',debug="True")
+    #app.run(port='80', debug="True")
